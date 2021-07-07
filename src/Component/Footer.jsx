987fefb2498ab9d0974b4e896d3d5db8c4027dc5
adityaparsad01 <adityaparsad01@gmail.com>
2021-07-07 13:58:25 +0530

    arranged

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -6,40 +6,8 @@ function Footer(props) {
     <footer>
       <div className="card-footer text-center">
         <div className="">
-          Copyright © 2020 by <strong>WaitOminute.</strong> All Rights Reserved.
-        </div>
-
-        <div className="">
-          <a
-            href="/"
-            className="github"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GitHub />
-          </a>
-
-          <a className="api" href="/" target="_blank" rel="noopener noreferrer">
-            <Database />
-          </a>
-
-          <a
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="twitter"
-          >
-            <Twitter />
-          </a>
-
-          <a
-            href="/"
-            className="mail"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Mail />
-          </a>
+          Copyright © 2021 by <strong>Vikalshya Enterprise.</strong> All Rights
+          Reserved.
         </div>
       </div>
     </footer>
