68b4760d3de906338cb65253c2ebbe58dd4d07f6
adityaparsad01 <adityaparsad01@gmail.com>
2021-07-07 13:43:39 +0530

    arranged

--- src/ProductContext.js
+++ src/ProductContext.js
@@ -2,7 +2,10 @@ import React,{createContext, useState,useEffect} from 'react'
 
 export const ProductContext = createContext()
 
-const URL ="https://api.airtable.com/v0/appZMUOKDaPymOc6w/products?api_key=keyPPTmvOpAtkL49Q";
+const API = process.env.REACT_APP_VILAKSHYA_KEY
+const BASE = process.env.REACT_APP_VILAKSHYA_BASE
+
+const URL =`https://api.airtable.com/v0/${BASE}/products?api_key=${API}`;
 export const ProductProvider = ({children})=>{
     const [product, setProduct] = useState([])
 
